22b10673b595481e67cc55295554795895e0b1ce
alcestide <angpanariti@gmail.com>
2023-05-27 09:47:02 +0200

    Update

--- quicksort.lisp
+++ quicksort.lisp
@@ -10,3 +10,19 @@
       (quicksort (subseq vec 0 pivot-i) comp)
       (quicksort (subseq vec (1+ pivot-i)) comp)))
   vec)
+#|
+(defun quicksort (vec 1 r)
+  (let ((i 1)
+        1 (J r)
+        (p (svref vec (round (+ 1 r) 2))))
+      (while (<= i j)
+        (while (< (svref vec i) p) (incf i))
+        (while (> (svref vec j) p) (decf j))
+        (when (<= i j)
+          (rotatef (svref vec i) (svref vec j))
+          (incf i)
+          (decf j)))
+    (if (> (- j 1) 1) (quicksort vec 1 j))
+    (if (> (- r i) 1) (quicksort vec i r))))
+vec
+#|
