a99110806cc31330e35313b58e044ef5f13d37dc
alcestide <angpanariti@gmail.com>
2023-06-09 21:08:17 +0200

    ironclad_blowfish

---
+++ ironclad_blowfish.lisp
@@ -0,0 +1,23 @@
+(ql:quickload :ironclad)
+(defun get-cipher (key)
+  (ironclad:make-cipher :blowfish
+    :mode :ecb
+    :key (ironclad:ascii-string-to-byte-array key)))
+
+(defun encrypt (plaintext key)
+  (let ((cipher (get-cipher key))
+        (msg (ironclad:ascii-string-to-byte-array plaintext)))
+    (ironclad:encrypt-in-place cipher msg)
+    (ironclad:octets-to-integer msg)))
+
+(defun decrypt (ciphertext-int key)
+  (let ((cipher (get-cipher key))
+        (msg (ironclad:integer-to-octets ciphertext-int)))
+    (ironclad:decrypt-in-place cipher msg)
+    (coerce (mapcar #'code-char (coerce msg 'list)) 'string)))
+
+;; Encryption
+;; (defvar encrypted-message (encrypt "Message" "Password"))
+
+;; Decryption
+;; (decrypt encrypted-message "Password")
